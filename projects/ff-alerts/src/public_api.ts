e998665c64a0c8b917bd04b8b909f13f18b2b35d
Mathijs Blok <mathijs@full-fledged.nl>
2021-11-17 14:43:57 +0100

    update dependencies

--- projects/ff-alerts/src/public_api.ts
+++ projects/ff-alerts/src/public_api.ts
@@ -1,4 +1,5 @@
 export * from './lib/model/alert.model';
 export * from './lib/model/alert-config.model';
 export * from './lib/service/alert.service';
+export * from './lib/component/alert.component';
 export * from './lib/alert.module';
